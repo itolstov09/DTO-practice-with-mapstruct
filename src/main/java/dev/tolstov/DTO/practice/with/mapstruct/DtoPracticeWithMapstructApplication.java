49755b3a04f830085bffc5d3caa0def68e93e679
itolstov09 <itolstov09@gmail.com>
2023-09-06 21:47:16 +0600

    FC

---
+++ src/main/java/dev/tolstov/DTO/practice/with/mapstruct/DtoPracticeWithMapstructApplication.java
@@ -0,0 +1,13 @@
+package dev.tolstov.DTO.practice.with.mapstruct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DtoPracticeWithMapstructApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DtoPracticeWithMapstructApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/tolstov/DTO/practice/with/mapstruct/DtoPracticeWithMapstructApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.tolstov.DTO.practice.with.mapstruct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DtoPracticeWithMapstructApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
